d03ae6eaa3c89cd97b73724794e450755de43f86
ot664vts <luna.colombanfernandez@ynov.com>
2025-02-02 23:43:29 +0100

    merge

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             // stop le jeu
             Time.timeScale = 0;
+            SceneManager.LoadScence("GameOver")
         }
     }
 }
